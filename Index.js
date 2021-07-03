62bd79bcc3bec1ffd498423df4bb704ab2b89dd9
manish-glitch <manishshingare573@gmail.com>
2021-07-04 01:52:42 +0530

    added delete feature

--- Index.js
+++ Index.js
@@ -1,12 +1,14 @@
 const taskContainer = document.querySelector(".task__container");
 
-const globaStore = [];
+var globalStore = [];
 
-const generateNewCard = (taskData) =>`<div class="col-md-6 col-lg-4 mt-5" id=${taskData.id}>
+const generateNewCard = (taskData) =>`
+<div class="col-md-6 col-lg-4 mt-5" id=${taskData.id}>
 <div class="card ">
     <div class="card-header d-flex justify-content-end gap-1">
         <button type="button" class="btn btn-outline-success"><i class="fas fa-pencil-alt"></i></button>
-        <button type="button" class="btn btn-outline-danger"><i class="fas fa-trash-alt"></i></button>
+        <button type="button" class="btn btn-outline-danger" id=${taskData.id} onclick="deletCard.apply(this, arguments)">
+        <i class="fas fa-trash-alt" id=${taskData.id} onclick="deletCard.apply(this, arguments)"></i></button>
     </div>
     <img src= ${taskData.imageUrl} class="card-img-top" alt="task_image">
     <div class="card-body">
@@ -22,15 +24,16 @@ const generateNewCard = (taskData) =>`<div class="col-md-6 col-lg-4 mt-5" id=${t
 </div>`
 
 const loadInitialCardData = () =>{
-    //localstorage to get tasky card data
+    
     const getCardData= localStorage.getItem("tasky");
     //convert to normal object
     const {cards} = JSON.parse(getCardData);
-    //loop over those array of task object to create html card, inject it to dom
+    //loop over those array of task object to create html card,
     cards.map((cardObject) =>{
+        // inject it to dom
         taskContainer.insertAdjacentHTML("beforeend", generateNewCard(cardObject) );
           // update our globalstorage
-        globaStore.push(cardObject);
+        globalStore.push(cardObject);
     })
   
 }
@@ -45,8 +48,27 @@ const saveChanges = () =>{
 
     taskContainer.insertAdjacentHTML("beforeend", generateNewCard(taskData) );
 
-    globaStore.push(taskData);
+    globalStore.push(taskData);
 
-    localStorage.setItem("tasky", JSON.stringify({cards:globaStore}));
+    localStorage.setItem("tasky", JSON.stringify({cards:globalStore}));
 };
 
+const deletCard = (event) =>{
+
+    event = window.event;
+
+    const targetId = event.target.id;
+    const tagname= event.target.tagName;
+
+
+
+    globalStore= globalStore.filter((cardObject)=> cardObject.id !==targetId);
+    localStorage.setItem("tasky", JSON.stringify({cards:globalStore}));
+    
+
+    if(tagname === "BUTTON"){
+        return taskContainer.removeChild(event.target.parentNode.parentNode.parentNode);
+    }else{
+        return taskContainer.removeChild(event.target.parentNode.parentNode.parentNode.parentNode);
+    }
+}
